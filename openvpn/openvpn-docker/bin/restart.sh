f1c84b6fc512740b59a8198ba9297980f2f38c52
d3vilh <d3vilh@gmail.com>
2023-01-09 22:43:41 +0200

    bugfix for restart.sh

--- openvpn/openvpn-docker/bin/restart.sh
+++ openvpn/openvpn-docker/bin/restart.sh
@@ -2,9 +2,11 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Directory where OpenVPN configuration files are stored
-OVDIR=/etc/openvpn
-CONTAINER_ID=$(curl --unix-socket /var/run/docker.sock "http:/v1.40/containers/json?filters={\"name\":[\"openvpn\"]}" | grep -oP '(?<="Id":")[^"]*')
-# Change to the /opt directory
-#cd /opt/
-curl --unix-socket /var/run/docker.sock -X POST "http:/v1.40/containers/$CONTAINER_ID/restart"
+# Get the container ID for ^openvpn$
+CONTAINER_ID=$(curl --unix-socket /var/run/docker.sock "http://v1.40/containers/json?filters=%7B%22name%22%3A%5B%22%5Eopenvpn$%22%5D%7D" | grep '"Id":' | cut -d '"' -f 4)
+
+# Restart the container
+curl --unix-socket /var/run/docker.sock -X POST "http://v1.40/containers/$CONTAINER_ID/restart"
+
+# Print the restarted container ID
+echo "Restarted container $CONTAINER_ID"
