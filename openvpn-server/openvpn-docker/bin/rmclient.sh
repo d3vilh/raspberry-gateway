aa11134fd750e97864a38383126b302f5c6f5722
Mr. Φίλιππος <33333559+d3vilh@users.noreply.github.com>
2023-07-30 17:08:44 +0300

    corrupts config files if VPN name clients has the same beginning

--- openvpn-server/openvpn-docker/bin/rmclient.sh
+++ openvpn-server/openvpn-docker/bin/rmclient.sh
@@ -2,42 +2,36 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Set the path of the OpenVPN configuration file for the specified user
+# .ovpn file path
 DEST_FILE_PATH="/etc/openvpn/clients/$1.ovpn"
 
-# Check if the OpenVPN configuration file exists
+# Check if .ovpn file exists
 if [[ ! -f $DEST_FILE_PATH ]]; then
     echo "User not found."
     exit 1
 fi
 
-# Modify the index.txt file by removing everything after pattern "/name=$1" in the line
-sed -i'.bak' "s/\/name=${1}.*//" /usr/share/easy-rsa/pki/index.txt
+# Fix index.txt by removing everything after pattern "/name=$1" in the line
+# Fix for https://github.com/d3vilh/openvpn-ui/issues/5 by shuricksumy@github
+sed -i'.bak' "s/\/name=${1}\/.*//" /usr/share/easy-rsa/pki/index.txt
 
-# Set the EASYRSA_BATCH variable to enable non-interactive mode for easy-rsa
 export EASYRSA_BATCH=1 # see https://superuser.com/questions/1331293/easy-rsa-v3-execute-build-ca-and-gen-req-silently
 
 echo 'Revoke certificate...'
 
-# Change to the easy-rsa directory and copy the easy-rsa variables file
+# Copy easy-rsa variables
 cd /usr/share/easy-rsa
 cp /etc/openvpn/config/easy-rsa.vars ./vars
 
-# Revoke the specified user's certificate
+# Revoke certificate
 ./easyrsa revoke "$1"
 
-# Generate a new CRL
 echo 'Create new Create certificate revocation list (CRL)...'
 ./easyrsa gen-crl
-
-# Make the CRL file readable
 chmod +r ./pki/crl.pem
 
 echo 'Sync pki directory...'
-# Remove all files in the /etc/openvpn/pki directory
 #rm -rf /etc/openvpn/pki/*
-
-# Copy all files from the easy-rsa PKI directory to the OpenVPN PKI directory
 cp -r ./pki/. /etc/openvpn/pki
 
 echo 'Done!'
