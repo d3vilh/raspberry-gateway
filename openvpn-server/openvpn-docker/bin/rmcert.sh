c709f1ce83c484447639494ca5217cb9e9d0f938
Mr. Φίλιππος <33333559+d3vilh@users.noreply.github.com>
2023-09-14 12:30:47 +0300

    openvpn-server scripts tuning

--- openvpn-server/openvpn-docker/bin/rmcert.sh
+++ openvpn-server/openvpn-docker/bin/rmcert.sh
@@ -3,38 +3,39 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# .ovpn file path
-DEST_FILE_PATH="/etc/openvpn/clients/$1.ovpn"
-INDEX_PATH="/usr/share/easy-rsa/pki/index.txt"
+#Variables
+CERT_NAME=$1
+CERT_SERIAL=$2
+EASY_RSA="/usr/share/easy-rsa"
+OPENVPN_DIR=/etc/openvpn
+INDEX=$EASY_RSA/pki/index.txt
+OVPN_FILE_PATH="$OPENVPN_DIR/clients/$CERT_NAME.ovpn"
 
 # Check if .ovpn file exists
-if [[ ! -f $DEST_FILE_PATH ]]; then
+if [[ ! -f $OVPN_FILE_PATH ]]; then
     echo "User not found."
     exit 1
 fi
 
 # Define key serial number by keyname
 # Fix for https://github.com/d3vilh/openvpn-ui/issues/5 by shuricksumy@github
-STATUS_CH=$(grep -e ${1}$ -e${1}/ ${INDEX_PATH} | awk '{print $1}' | tr -d '\n')
+STATUS_CH=$(grep -e ${CERT_NAME}$ -e${CERT_NAME}/ ${INDEX} | awk '{print $1}' | tr -d '\n')
 if [[ $STATUS_CH = "V" ]]; then
-    echo "Cert is VALID"
-    CERT_SERIAL=$(grep ${1}/ ${INDEX_PATH} | awk '{print $3}' | tr -d '\n')
-    echo "Will remove: ${CERT_SERIAL}"
+    echo "Cert is VALID\nShould not remove: $CERT_NAME with serial: $CERT_SERIAL\nExiting..."
+    exit 1
 else
-    echo "Cert is REVOKED"
-    CERT_SERIAL=$(grep ${1}$ ${INDEX_PATH} | awk '{print $4}' | tr -d '\n')
-    echo "Will remove: ${CERT_SERIAL}"
+    echo "Cert is REVOKED\nContinue to remove: $CERT_NAME with serial: $CERT_SERIAL"
 fi
 
 # Remove user from OpenVPN
 rm -f /etc/openvpn/pki/certs_by_serial/$CERT_SERIAL.pem
-rm -f /etc/openvpn/pki/issued/$1.crt
-rm -f /etc/openvpn/pki/private/$1.key
-rm -f /etc/openvpn/pki/reqs/$1.req
-rm -f /etc/openvpn/clients/$1.ovpn
+rm -f /etc/openvpn/pki/issued/$CERT_NAME.crt
+rm -f /etc/openvpn/pki/private/$CERT_NAME.key
+rm -f /etc/openvpn/pki/reqs/$CERT_NAME.req
+rm -f /etc/openvpn/clients/$CERT_NAME.ovpn
 
 # Fix index.txt by removing the user from the list following the serial number
-sed -i'.bak' "/${CERT_SERIAL}/d" $INDEX_PATH
+sed -i'.bak' "/${CERT_SERIAL}/d" $INDEX
+echo "Database fixed."
 
-echo 'Remove done!'
-echo 'If you want to disconnect the user please restart the service using docker-compose restart openvpn.'
+echo 'Remove done!\nIf you want to disconnect the user please restart the OpenVPN service or container.'
